e30b5c88e310e6b7260ee27ecf58f6055efc8e53
lucron9090 <153255025+lucron9090@users.noreply.github.com>
2024-03-05 10:06:24 -0500

    v1

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+
+      <q-toolbar-title>
+        CS Assessment Game
+      </q-toolbar-title>
+
+      <div>Quasar v{{ $q.version }}</div>
+
+    </q-header>
+
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+  },
+
+  setup() {
+
+
+    return {
+
+    }
+  }
+})
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,69 @@
+<template>
+  <q-page>
+    <div id="game-container">
+      <div id="flashcard" v-if="!showResult">
+        <p id="equation">{{ equation }}</p>
+        <q-input v-model="answer" type="number" />
+        <q-btn @click="check" label="Submit" />
+      </div>
+      <div id="result-screen" v-if="showResult">
+        <p id="result" :style="{ color: color }">{{ msg }}</p>
+        <q-btn @click="restart" label="Restart" />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const equation = ref('');
+    const answer = ref(null);
+    const showResult = ref(false);
+    const msg = ref('');
+    const color = ref('');
+
+    function generate() {
+      let n1 = Math.floor(Math.random() * 5) + 1;
+      let n2 = Math.floor(Math.random() * 5) + 1;
+      let op = Math.random() < 0.5 ? '+' : '*';
+      equation.value = `${n1} ${op} ${n2}`;
+      answer.value = null;
+    }
+
+    function check() {
+      let correct = eval(equation.value);
+
+      if (Number(answer.value) === correct) {
+        msg.value = 'Correct!';
+        color.value = 'green';
+      } else {
+        msg.value = 'Incorrect!';
+        color.value = 'red';
+      }
+
+      showResult.value = true;
+    }
+
+    function restart() {
+      showResult.value = false;
+      generate();
+    }
+
+    generate();
+
+    return {
+      equation,
+      answer,
+      showResult,
+      msg,
+      color,
+      generate,
+      check,
+      restart,
+    };
+  },
+};
+</script>
